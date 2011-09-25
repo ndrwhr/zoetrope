d581327ea13f4e4a6493b0cb36489157044a373a
Andrew Hoyer <andrew@inkling.com>
2011-09-25 09:41:18 -0700

    Adding slide image and tweaking speeds

--- generator.rb
+++ generator.rb
@@ -9,6 +9,7 @@ angleStep = 360 / frames
 	output.puts <<-css
 .frames > div:nth-child(#{ i + 1 }) {
 	-webkit-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+    background-position: #{ 100 * i }px 0;
 }
 	css
 end
