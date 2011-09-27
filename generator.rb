fd6cd02d1b2258a979c4f1ebdb99e7e97a32dbf6
Andrew Hoyer <andrew@inkling.com>
2011-09-26 22:25:36 -0700

    Adding cross browser prefixes to css

--- generator.rb
+++ generator.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/ruby
 
-output = File.open('assets/frames.css', 'w')
+output = File.open('assets/css/frames.css', 'w')
 
 frames = 24
 angleStep = 360 / frames
@@ -8,7 +8,11 @@ angleStep = 360 / frames
 (0...frames).each do |i|
 	output.puts <<-css
 .frames > div:nth-child(#{ i + 1 }) {
-	-webkit-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+	transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+    -webkit-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+    -moz-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+    -ie-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+    -o-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
     background-position: #{ 100 * i }px 0;
 }
 	css
