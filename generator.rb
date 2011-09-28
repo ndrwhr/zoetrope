5f4046bbc54fc93e0acd65bd05e256a940344ce6
Andrew Hoyer <andrew@inkling.com>
2011-09-28 08:57:32 -0700

    Getting rid of hard tabs from generator script.

--- generator.rb
+++ generator.rb
@@ -6,14 +6,14 @@ frames = 24
 angleStep = 360 / frames
 
 (0...frames).each do |i|
-	output.puts <<-css
+    output.puts <<-css
 .frames > div:nth-child(#{ i + 1 }) {
-	transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
+    transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
     -webkit-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
     -moz-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
     -ie-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
     -o-transform: rotateY(#{ angleStep * i }deg) translateZ(380px);
     background-position: #{ 100 * i }px 0;
 }
-	css
+    css
 end
